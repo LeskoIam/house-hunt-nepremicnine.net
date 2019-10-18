190c3a2e1978ee0af666ed082f31f8ac9d6bc76c
Lesko <polensek.matevz@gmail.com>
2019-10-18 19:44:14 +0200

    Widened the search.

--- househunt/househunt/spiders/constants.py
+++ househunt/househunt/spiders/constants.py
@@ -4,10 +4,12 @@
 # When it lies to you, it may be a while before you realize something's wrong.
 
 DOMAIN_NEPREMICNINE = "nepremicnine.net"
-URL_NEPREMICNINE = "https://www.nepremicnine.net/oglasi-prodaja/ljubljana-okolica/domzale,grosuplje,kamnik,litija,logatec,vrhnika,ljubljana-severovzhodni-del-bezigrad,ljubljana-jugozahodni-del-vic-rudnik,ljubljana-severozahodni-del-siska/hisa/samostojna/cena-do-150000-eur,zemljisce-od-600-m2/?s=13"
+URL_NEPREMICNINE = "https://www.nepremicnine.net/oglasi-prodaja/ljubljana-okolica/domzale,grosuplje,kamnik,litija,logatec,vrhnika,ljubljana-severovzhodni-del-bezigrad,ljubljana-vzhodni-del-moste-polje,ljubljana-jugozahodni-del-vic-rudnik,ljubljana-severozahodni-del-siska/hisa/samostojna,vrstna,atrijska,dvojcek,dvostanovanjska,trojcek,drugo-37/cena-do-150000-eur,zemljisce-od-500-m2/"
+# URL_NEPREMICNINE = "https://www.nepremicnine.net/oglasi-prodaja/ljubljana-okolica/domzale,grosuplje,kamnik,litija,logatec,vrhnika,ljubljana-severovzhodni-del-bezigrad,ljubljana-jugozahodni-del-vic-rudnik,ljubljana-severozahodni-del-siska/hisa/samostojna/cena-do-150000-eur,zemljisce-od-600-m2/?s=13"
 # URL = "https://www.nepremicnine.net/oglasi-prodaja/ljubljana-okolica/domzale,grosuplje,kamnik,litija,ljubljana-jugozahodni-del-vic-rudnik,ljubljana-severovzhodni-del-bezigrad,ljubljana-severozahodni-del-siska,ljubljana-vzhodni-del-moste-polje,logatec,vrhnika/hisa/samostojna,vrstna,atrijska,dvojcek,dvostanovanjska,trojcek,drugo-37/"
 
 
 DOMAIN_BOLHA = "bolha.com"
+URL_BOLHA = "http://www.bolha.com/nepremicnine/hise/?reType=monta%C5%BEna%7C%7Csamostojna%7C%7Costalo&viewType=30&adTypeH=00_Prodam%2FS+fiksno+ceno%2F&priceSortField=0%7C150000&reSize=60%7C8223&reSizeExtra=600%7C170000&location=Jugovzhodna+Slovenija%2F%7C%7COsrednjeslovenska%2F%7C%7CSpodnjeposavska%2F%7C%7CZasavska%2F%7C%7CSavinjska%2F%7C%7CGorenjska%2F%7C%7CNotranjsko-kra%C5%A1ka%2F%7C%7CPomurska%2F%7C%7CGori%C5%A1ka%2F%7C%7CKoro%C5%A1ka%2F%7C%7CObalno-kra%C5%A1ka%2F%7C%7CPodravska%2F&hasImages=Oglasi+s+fotografijami"
+# URL_BOLHA = "http://www.bolha.com/nepremicnine/hise?reType=monta%C5%BEna%7C%7Csamostojna%7C%7Costalo&viewType=30&adTypeH=00_Prodam/&priceSortField=0%7C150000&reSize=70%7C8223&reSizeExtra=600%7C170000&location=Jugovzhodna%20Slovenija/%7C%7COsrednjeslovenska/%7C%7CSpodnjeposavska/%7C%7CZasavska/%7C%7CSavinjska/"
 # URL_BOLHA = "http://www.bolha.com/nepremicnine/hise?reType=monta%C5%BEna%7C%7Csamostojna&location=Osrednjeslovenska/%7C%7CJugovzhodna%20Slovenija/%7C%7CNotranjsko-kra%C5%A1ka/&viewType=30&adTypeH=00_Prodam/&priceSortField=0%7C150000&reSize=70%7C8223&reSizeExtra=600%7C14289"
-URL_BOLHA = "http://www.bolha.com/nepremicnine/hise?reType=monta%C5%BEna%7C%7Csamostojna%7C%7Costalo&viewType=30&adTypeH=00_Prodam/&priceSortField=0%7C150000&reSize=70%7C8223&reSizeExtra=600%7C170000&location=Jugovzhodna%20Slovenija/%7C%7COsrednjeslovenska/%7C%7CSpodnjeposavska/%7C%7CZasavska/%7C%7CSavinjska/"
